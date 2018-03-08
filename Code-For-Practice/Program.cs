d6f45559c180988efb710775d8284c104ed537a9
jiwei sung <jiweisung@outlook.com>
2018-03-09 00:19:37 +0800

    添加项目文件。

---
+++ Code-For-Practice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code_For_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
